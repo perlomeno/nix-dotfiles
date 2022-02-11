c3e6cb89264ba84c0b1582f7067c590f29ff3de3
Alessandro Perlo <aleperlo002@gmail.com>
2022-02-11 13:41:34 +0100

    Added /etc/nixos

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo -e "1) desktop\n2) thinkbook"
+while :
+do
+	echo -n "Which host do you want to configure? "
+	read HOST_NUMBER
+	
+	case $HOST_NUMBER in	
+		1)
+			NAME="ap-desktop"
+			break
+			;;			
+		2)
+			NAME="thinkbook"
+			break
+			;;			
+		*)
+			echo -n "Unknown value. "
+			;;
+	esac
+done
+
+echo "Cleaning /etc/nixos"
+sudo rm /etc/nixos/*
+echo "Copying configuration.nix to /etc/nixos"
+sudo cp nixos/configuration.nix /etc/nixos
+echo -e "Copying $NAME folder to /etc/nixos"
+sudo cp nixos/$NAME/* /etc/nixos
---
+++ nixos/configuration.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ./device.nix
+    ];
+
+  #
+  # Boot loader.
+  #
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  #
+  # Networking
+  #
+
+  networking.networkmanager.enable = true;
+
+  #
+  # Localization
+  #
+
+  time.timeZone = "Europe/Rome";
+  i18n.defaultLocale = "it_IT.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "it";
+  };
+
+  #
+  # Desktop
+  #
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    layout = "it";
+    xkbOptions = "ctrl:swapcaps";
+    libinput.enable = true;
+  };
+
+  #
+  # Users
+  #
+
+  users.users.ap = {
+    description = "Alessandro Perlo";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+  };
+
+  #
+  # Packages
+  #
+
+  environment.systemPackages = with pkgs; [
+    chromium
+    git
+    virt-manager
+  ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany
+    gnome-tour
+    gnome.gedit
+    gnome.gnome-contacts
+    gnome.gnome-maps
+    gnome.gnome-music
+    gnome.gnome-weather
+    gnome-photos
+    gnome.totem
+    gnome.yelp
+  ];
+
+  programs.neovim = {
+    enable = true;
+    configure = {
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [vim-nix];
+      };
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  #
+  # Services
+  #
+
+  virtualisation.libvirtd.enable = true;
+
+  services.openssh.enable = true;
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.epson-escpr ];
+  };
+
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
+
+  system.stateVersion = "21.11";
+
+}
+
