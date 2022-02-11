3e76a3e6c7a7efb2e8e09b4cf42681f29d83ff5d
Alessandro Perlo <aleperlo002@gmail.com>
2022-02-11 18:59:28 +0100

    Added syncthing

--- nixpkgs/modules/packages.nix
+++ nixpkgs/modules/packages.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 {
+
   home.packages = with pkgs; [
     # office
     calibre
@@ -26,5 +27,9 @@
     tdesktop
     zoom-us
   ];
+
+  services.syncthing = {
+    enable = true;
+  };
 
 }
