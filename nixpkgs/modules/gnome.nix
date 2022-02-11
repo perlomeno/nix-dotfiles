3266aa2d3e01cd2d006911ea29810e97f0ae4ce3
Alessandro Perlo <aleperlo002@gmail.com>
2022-02-11 18:59:17 +0100

    Set wallpaper

--- nixpkgs/modules/gnome.nix
+++ nixpkgs/modules/gnome.nix
@@ -2,10 +2,12 @@
 
 {
   dconf.settings = {
+  
     "org/gnome/settings-daemon/plugins/color" = {
       night-light-enabled = true;
       night-light-temperature = "uint32 5500";
     };
+    
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
@@ -18,5 +20,11 @@
       ];
       app-picker-layout = [];
     };
+
+    "org/gnome/desktop/background" = {
+      picture-uri = "file:///home/ap/Dotfiles/nixpkgs/pictures/wallpaper.png";
+      picture-options = "zoom";
+    };
+
   };
 }
