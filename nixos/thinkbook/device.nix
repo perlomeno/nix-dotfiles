7c6c8bc3afcbaa4b5a7adb25f29ac17eea7a6f47
Alessandro Perlo <aleperlo002@gmail.com>
2022-02-12 19:36:13 +0100

    Added grub to thinkbook configuration

--- nixos/thinkbook/device.nix
+++ nixos/thinkbook/device.nix
@@ -1,5 +1,20 @@
 { config, pkgs, ... }:
 
 {
+
   networking.hostName = "thinkbook";
+
+  boot.loader = {
+    efi.canTouchEfiVariables = false;
+    grub = {
+      enable = true;
+      version = 2;
+      device = "nodev";
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      useOSProber = true;
+      enableCryptodisk = true;
+    };
+  };
+
 }
