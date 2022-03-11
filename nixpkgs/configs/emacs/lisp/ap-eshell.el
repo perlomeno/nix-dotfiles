f29913a6e44210695af2531c0da2fc7625591044
Alessandro Perlo <aleperlo002@gmail.com>
2022-03-11 18:38:16 +0100

    Configured eshell

--- nixpkgs/configs/emacs/lisp/ap-eshell.el
+++ nixpkgs/configs/emacs/lisp/ap-eshell.el
@@ -1,11 +1,16 @@
 (use-package eshell
   :hook
   (eshell-mode . (lambda ()
-		   (display-line-numbers-mode 0)))
-  (eshell-mode . (lambda ()
-		   (setq-local global-hl-line-mode nil)))
+		   (display-line-numbers-mode 0)
+		   (setq-local global-hl-line-mode nil)
+		   (setq-local corfu-auto nil)
+		   (corfu-mode)))
   :config
   (setq eshell-hist-ignoredups t)
-  (setq eshell-scroll-to-bottom-on-input t))
+  (setq eshell-cmpl-ignore-case t))
+
+(use-package eshell-git-prompt
+  :config
+  (eshell-git-prompt-use-theme 'git-radar))
 
 (provide 'ap-eshell)
