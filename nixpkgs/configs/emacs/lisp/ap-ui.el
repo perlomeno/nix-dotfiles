c56ea6f83f0192614a3eb8a3afa651561380e672
Alessandro Perlo <aleperlo002@gmail.com>
2022-03-12 11:06:47 +0100

    Switched to moody

--- nixpkgs/configs/emacs/lisp/ap-ui.el
+++ nixpkgs/configs/emacs/lisp/ap-ui.el
@@ -11,9 +11,23 @@
 ;; Theme configuration
 (use-package color-theme-sanityinc-tomorrow
   :config
-  (load-theme 'sanityinc-tomorrow-day t))
+  (load-theme 'sanityinc-tomorrow-day t)
+  (let ((line (face-attribute 'mode-line :underline)))
+    (set-face-attribute 'mode-line          nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :underline  line)
+    (set-face-attribute 'mode-line          nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :box        nil)))
 
 ;; Modeline configuration
+(use-package moody
+  :config
+  (setq moody-mode-line-height 50)
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (moody-replace-eldoc-minibuffer-message-function))
+
 (use-package minions
   :config
   (setq minions-mode-line-lighter "~")
