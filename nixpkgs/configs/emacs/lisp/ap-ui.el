0d8e4d7bbd3d6243f0b888f3508f70de73130003
Alessandro Perlo <aleperlo002@gmail.com>
2022-03-24 11:17:01 +0100

    Removed load average

--- nixpkgs/configs/emacs/lisp/ap-ui.el
+++ nixpkgs/configs/emacs/lisp/ap-ui.el
@@ -38,6 +38,7 @@
 (column-number-mode 1)
 (display-time-mode 1)
 (setq display-time-24hr-format t)
+(setq display-time-default-load-average nil)
 
 ;; Font configuration
 (set-face-attribute 'default nil :font "Iosevka" :height 130)
