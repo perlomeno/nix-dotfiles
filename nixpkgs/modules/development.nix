841802db7806c4a450eaf103283213e01ae42a7b
Alessandro Perlo <aleperlo002@gmail.com>
2022-04-06 22:00:40 +0200

    Revert "Added emacs overlay"
    This reverts commit a327155fc4bd42fc3ff1b1905e35c5870ef7c441.

--- nixpkgs/modules/development.nix
+++ nixpkgs/modules/development.nix
@@ -27,14 +27,7 @@
   # Emacs
   #
 
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-    }))
-  ];
-
   programs.emacs = {
-    package = pkgs.emacsUnstable;
     enable = true;
   };
 
